6b1602ff6a57ebfb9e0e239f08940757d88d4fa4
BrunaGPires <bruna.gpires@hotmail.com>
2023-11-28 21:45:13 -0300

    first commit

---
+++ Converter/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+List<int> resultado = new List<int>();
+while (numero > 0 )
+{
+    int resto = numero % 2;
+    resultado.Insert(0, resto);
+    numero /= 2;
+}
+
+while (resultado.Count < 8)
+{
+    resultado.Insert(0, 0);
+}
+
+Console.WriteLine($"O número {numero} em binário é:");
+foreach (int valor in resultado)
+{
+    Console.Write(valor);
+}
